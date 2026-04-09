132765c01f228a421e32413f93615df0e4df730b
Ebrahim Hamza <ebrahim5529@gmail.com>
2026-04-09 07:21:04 +0200

    favicon
    favicon

--- bigbluebutton-html5/imports/ui/stylesheets/styled-components/globalStyles.js
+++ bigbluebutton-html5/imports/ui/stylesheets/styled-components/globalStyles.js
@@ -102,7 +102,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   .toastBodyClass {
-    font-family: 'Source Sans Pro';
+    font-family: 'Noto Sans Arabic', 'Source Sans Pro';
     margin: auto auto;
     flex: 1;
     background-color: inherit;
