6984f283764db8e90beb628f281f5f0cd9b47c49
Ebrahim Hamza <ebrahim5529@gmail.com>
2026-04-09 08:42:42 +0200

    إتمام تعريب المنصة بالكامل (Greenlight v3) وتحديث الهوية البصرية لمنصة أسهل التعليمية وإصلاح تداخل RTL

--- greenlight/config/initializers/locales.rb
+++ greenlight/config/initializers/locales.rb
@@ -19,4 +19,4 @@
 # Default language initialization.
 # The default language will be used to define the default language config for all users
 # that GL coudn't infer their preferred language from their browsers.
-I18n.default_locale = ENV.fetch('DEFAULT_LOCALE', 'en')
+I18n.default_locale = ENV.fetch('DEFAULT_LOCALE', 'ar')
